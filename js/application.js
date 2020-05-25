748044a82a0fa3456571dde72087d982fbb69028
TheReal23 <65376048+TheReal23@users.noreply.github.com>
2020-05-25 08:36:58 -0700

    merge common js into one file

--- js/application.js
+++ js/application.js
@@ -1,5 +1,5 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  window.gameName = '2048-advanced';
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  window.gameName = '2048-advanced-plus';
+  new GameManager(8, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
